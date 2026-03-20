22dc32d6570f58087dbc2ad4101f5cd76ad185b8
Emil Djupvik <emil@djupvik.dev>
2026-03-20 09:55:14 +0000

    add a cpp package

--- ros2_ws/src/robot_bringup/launch/all_nodes.launch.py
+++ ros2_ws/src/robot_bringup/launch/all_nodes.launch.py
@@ -3,15 +3,31 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    return LaunchDescription([
-        Node(
-            package='publisher_pkg',
-            executable='greeter_node',
-        ),
-        Node(
-            package='subscriber_pkg',
-            executable='listener_node',
-            respawn=True,
-            respawn_delay=2.0,
-        ),
-    ])
+    return LaunchDescription(
+        [
+            Node(
+                package="publisher_pkg",
+                executable="greeter_node",
+                respawn=True,
+                respawn_delay=2.0,
+            ),
+            Node(
+                package="subscriber_pkg",
+                executable="listener_node",
+                respawn=True,
+                respawn_delay=2.0,
+            ),
+            Node(
+                package="subscriber_pkg",
+                executable="listener_node",
+                respawn=True,
+                respawn_delay=2.0,
+            ),
+            Node(
+                package="cpp_subscriber_pkg",
+                executable="listener_node",
+                respawn=True,
+                respawn_delay=2.0,
+            ),
+        ]
+    )
